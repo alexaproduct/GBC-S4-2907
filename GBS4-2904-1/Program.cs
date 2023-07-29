370f0a01353c3f3debbdf6f365a0326401e914d5
Alexandra Zotova <alexa@Alexandras-MacBook-Air-3.local>
2023-07-29 20:51:07 +0600

    Comment2

--- GBS4-2904-1/Program.cs
+++ GBS4-2904-1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив, заполнить числами 1 и 0.
+
+int[] arr = new int[8];
+
+for (int i = 0; i <= 7; i++)
+{
+    arr[i] = new Random().Next(0, 2);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
