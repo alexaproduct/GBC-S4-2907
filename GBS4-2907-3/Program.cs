57139b3f9c713e2bb698dbe9071b3241770cb9a3
Alexandra Zotova <alexa@Alexandras-MacBook-Air-3.local>
2023-07-30 18:21:14 +0600

    Comment8

---
+++ GBS4-2907-3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задан список имен. Обменяйте местами имена
+// первого и последнего, второго и предпоследнего,
+// третьего от начала и третьего с конца и т.д.
+
+
+string s = Console.ReadLine()??"";
+string[] names = s.Split(" ");
+
+
+void PrintMassString(string[] arr)
+{
+    foreach (var item in names)
+    {
+        Console.Write(item+ " ");
+    }
+}
+
+PrintMassString(names);
+Console.WriteLine();
+
+for (int i = 0; i < names.Length/2; i++)
+{
+    string temp = names[i];
+    names[i] = names[names.Length-1-i];
+    names[names.Length-1-i] = temp;
+}
+PrintMassString(names);
+Console.WriteLine();
+
+
+
