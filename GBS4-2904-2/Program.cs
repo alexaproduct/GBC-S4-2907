3f6d3579e01f2e4480d45e664cec1ed09ade2fd0
Alexandra Zotova <alexa@Alexandras-MacBook-Air-3.local>
2023-07-30 17:04:04 +0600

    Comment7

--- GBS4-2904-2/Program.cs
+++ GBS4-2904-2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дан одномерный массив целых чисел. 
+// Найдите, сколько раз в нем повторяется
+// самое частое число.
+// Например, 15 23 14 23 17 23 21 14 -> 3
+
+int[] arr = new int[8];
+
+for (int i = 0; i <= 7; i++)
+{
+    arr[i] = new Random().Next(14, 31);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
+
+int freq = 0;
+int maxfreq = 0;
+
+for (int i = 0; i <= 7; i++)
+{
+    for (int j = i+1; j <= 7; j++)
+    {
+        if (arr[i] == arr [j]) freq = freq + 1;
+    }
+    if (freq > maxfreq) maxfreq = freq;
+    else if (freq == maxfreq) maxfreq = freq;
+}
+
+Console.WriteLine(maxfreq);
+
+
