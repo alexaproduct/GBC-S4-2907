39f55e6cd412d91db06ec36a012e4905a8d54805
Alexandra Zotova <alexa@Alexandras-MacBook-Air-3.local>
2023-08-05 19:04:48 +0600

    Comment14

---
+++ GBS4-HW4-2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами.
+// Найдите сумму элементов,
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] arr = new int[5];
+
+for (int i = 0; i <= 4; i++)
+{
+    arr[i] = new Random().Next(0, 10);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
+
+int sumuneven = 0;
+
+for (int i = 0; i <= 4; i++)
+{
+    if (i%2 != 0) sumuneven = sumuneven + arr[i];
+}
+
+Console.WriteLine(sumuneven);
