8ebb975ff594ee9eadf9647667c6622e83840e36
Alexandra Zotova <alexa@Alexandras-MacBook-Air-3.local>
2023-08-06 15:49:32 +0600

    Comment16

--- GBL4-1/Program.cs
+++ GBL4-1/Program.cs
@@ -17,8 +17,7 @@ for (int rows = 0; rows < 2; rows++)
 Console.WriteLine();
 }
 */
-
-
+/*
 int[,] matrix = new int[3, 4];
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
@@ -28,5 +27,34 @@ for (int i = 0; i < matrix.GetLength(0); i++)
  }
 Console.WriteLine();
 }
+*/
+
 
 // Заполнение матрицы случайными числами
+
+void PrintArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ Console.Write($"{matr[i, j]} ");
+ }
+ Console.WriteLine();
+ }
+}
+void FillArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ matr[i,j] = new Random().Next(1,10);//[1; 10)
+ }
+ }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
